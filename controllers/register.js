6c42a487c83f347ac46b2cfd711a5e18adc03226
ylu8888 <yang.lu.3@stonybrook.edu>
2023-07-23 02:53:55 -0400

    added comments

--- controllers/register.js
+++ controllers/register.js
@@ -5,14 +5,17 @@ const bcrypt = require("bcryptjs");
 const register = async (req, res) => {
     //normal password saved in hash
     const {email, password: Npassword} = req.body
+    //checks if email and password fields are provided in the request body
     if(!email || !Npassword) return res.json({status:"error", error:"Enter your email and password"});
     else{
         console.log(email);
+        //checks if the email already exists in the sql database, which is set to unique emails
         db.query('SELECT email FROM users WHERE email = ?', [email], async (err, result) => {
             if (err) throw err;
             if (result[0]) return res.json({status:"error", error:"Email has already been registered"})
-            else{   //this hashes password at 8 chars, not too much load on server
-                const password = await bcrypt.hash(Npassword, 8);
+            //this portion below means the email was unique and registered successfully
+            else{  
+                const password = await bcrypt.hash(Npassword, 8);  //this hashes password at 8 chars, not too much load on server
                 console.log(password);
                 //insert value into the database
                 db.query('INSERT INTO users SET ?', { email: email, password: password}, (error, results) => {

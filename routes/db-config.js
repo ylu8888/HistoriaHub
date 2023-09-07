2515d3a05e704be8b8e034256da3212cb6887672
ylu <123523291+ylu8888@users.noreply.github.com>
2023-09-07 17:45:59 -0400

    login bug fixed!

--- routes/db-config.js
+++ routes/db-config.js
@@ -7,8 +7,43 @@ const db = sql.createConnection({
     database:process.env.DATABASE
 })
 
+// force mysql to keep connection alive, the login backend works AND mysql doesnt connect
 setInterval(function () {
-  db.query('SELECT 1');
+  db.query('SELECT 1'); //make a query every 5 seconds testing the DB connection
 }, 5000);
+
+/*recreate a connection method, doesnt work with the login 
+const db_config= {
+    host:process.env.DATABASE_HOST,
+    user:process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+    database:process.env.DATABASE
+}
+
+var db;
+
+function handleDisconnect() {
+  db = sql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  db.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  db.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+*/
 
 module.exports = db;
